ed238d4cdb73fdb2610aa2b0e0a749551901e8cc
wh498574932 <wh498574932@gmail.com>
2019-02-20 15:27:51 -0800

    Add Solution for LC001

---
+++ src/com/haowu/leetcode/lc001/TwoSum.java
@@ -0,0 +1,23 @@
+package com.haowu.leetcode.lc001;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        if(nums == null || nums.length < 1) { return null; }
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; ++i) {
+            if(map.containsKey(target-nums[i])) {
+                return new int[]{map.get(target-nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
