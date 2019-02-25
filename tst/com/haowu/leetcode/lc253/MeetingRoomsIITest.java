9e04f617b752ab21755f8bdf7b09f71c154a880c
wh498574932 <wh498574932@gmail.com>
2019-02-25 15:43:58 -0800

    Add O(N) Solution for LC253

--- tst/com/haowu/leetcode/lc253/MeetingRoomsIITest.java
+++ tst/com/haowu/leetcode/lc253/MeetingRoomsIITest.java
@@ -17,4 +17,16 @@ public class MeetingRoomsIITest {
         };
         assertEquals(2, new MeetingRoomsII().minMeetingRooms(intervals));
     }
+
+    @Test
+    public void test2() {
+        Interval[] intervals = new Interval[]{
+            new Interval(2, 15),
+            new Interval(36, 45),
+            new Interval(9, 29),
+            new Interval(16, 23),
+            new Interval(4, 9)
+        };
+        assertEquals(2, new MeetingRoomsII2().minMeetingRooms(intervals));
+    }
 }
