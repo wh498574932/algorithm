6d51981a2a9a7e98a8695dcf031389f81cef507e
wh498574932 <wh498574932@gmail.com>
2019-03-02 11:57:56 -0800

    Add Complexity Info

--- src/com/wh498574932/algorithm/lc278/FirstBadVersion.java
+++ src/com/wh498574932/algorithm/lc278/FirstBadVersion.java
@@ -10,6 +10,10 @@ package com.wh498574932.algorithm.lc278;
  *
  * You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to
  * find the first bad version. You should minimize the number of calls to the API.
+ *
+ * https://leetcode.com/problems/first-bad-version/
+ *
+ * Accepted	11 ms (99.93%)	34.4 MB (52.72%)
  */
 public class FirstBadVersion {
     public int firstBadVersion(int n) {
