36b37fe0277a838aa6221ef722362b502bdb0e08
wh498574932 <wh498574932@gmail.com>
2019-03-09 18:05:01 -0800

    Add Submission Info for LC001

--- src/com/wh498574932/algorithm/lc001/TwoSum.java
+++ src/com/wh498574932/algorithm/lc001/TwoSum.java
@@ -7,12 +7,18 @@ import java.util.Map;
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  *
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Time:    3 ms    99.55%  O(N)
+ *
+ * Space:   39 MB   28.07%  O(N)
+ *
+ * https://leetcode.com/problems/two-sum/
  */
 public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) { // O(N)
         if(nums == null || nums.length < 1) { return null; }
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; ++i) {
+        for(int i = 0; i < nums.length; ++i) { // O(N)
             if(map.containsKey(target-nums[i])) {
                 return new int[]{map.get(target-nums[i]), i};
             }
