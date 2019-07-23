ecfbaf2d1afdfa45a1bc009f5c7edbbcb576ec23
wh498574932 <wh498574932@gmail.com>
2019-07-23 14:55:00 -0700

    Add Solution for LC112

---
+++ src/com/wh498574932/algorithm/lc112/PathSum.java
@@ -0,0 +1,36 @@
+package com.wh498574932.algorithm.lc112;
+
+import com.wh498574932.algorithm.model.TreeNode;
+
+/**
+ * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along
+ * the path equals the given sum.
+ *
+ * Note: A leaf is a node with no children.
+ *
+ * Example:
+ *
+ * Given the below binary tree and sum = 22,
+ *
+ *       5
+ *      / \
+ *     4   8
+ *    /   / \
+ *   11  13  4
+ *  /  \      \
+ * 7    2      1
+ * return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+ *
+ * Time:        0 ms        100.00%     O(N)
+ * Space:       37.4 MB     99.84%      O(N)
+ *
+ * https://leetcode.com/problems/path-sum/
+ */
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if( root == null ) { return false; }
+        if( root.left == null && root.right == null ) { return sum == root.val; }
+        return root.left != null && hasPathSum(root.left, sum-root.val)
+            || root.right != null && hasPathSum(root.right, sum-root.val);
+    }
+}
